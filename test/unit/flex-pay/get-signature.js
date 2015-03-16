b5911fa24403237fade699014afb31d431ca142b
Zeph Lafassett <zeph@lafassett.com>
2015-03-15 17:38:09 -0700

    adding more tests, fixing errors for get signature

--- test/unit/flex-pay/get-signature.js
+++ test/unit/flex-pay/get-signature.js
@@ -5,11 +5,27 @@ var mockconfig = {
 			password: 'Token singlesignon'
 		}
 	},
-	FlexPay = require('../../../index')(mockconfig).FlexPay,
+	Flexpay = require('../../../index')(mockconfig).FlexPay,
 	should = require('should');
 
 describe('Flex Pay: Signature tests', function () {
 	'use strict';
+	var secret,
+		params;
+	beforeEach(function (done) {
+		secret = "my secret string";
+		params = {
+			custome: 'custome value',
+			description: 'description value',
+			priceAmount: '12.22',
+			priceCurrency: 'US',
+			referenceID: 'AAAAAAAAA',
+			saleID: '12AB',
+			shopID: 12345
+		};
+		done();
+	});
+
 	it('should fail if no parameters are supplied', function (done) {
 		(function () {
 			Flexpay.getSignature();
@@ -17,10 +33,32 @@ describe('Flex Pay: Signature tests', function () {
 		done();
 	});
 
-	it('should fail if signature parameters are supplied', function (done) {
+	it('should fail if `secret` argument is null', function (done) {
 		(function () {
-			Flexpay.getSignature(null);
+			Flexpay.getSignature(null, params);
 		}).should.throw();
 		done();
 	});
+
+	it('should fail if `params` argument is null', function (done) {
+		(function () {
+			Flexpay.getSignature(secret, null);
+		}).should.throw();
+		done();
+	});
+
+	it('should fail if `params` argument is an array', function (done) {
+		(function () {
+			Flexpay.getSignature(secret, []);
+		}).should.throw();
+		done();
+	});
+
+	it('should pass if everything is good', function (done) {
+		(function () {
+			Flexpay.getSignature(secret, params);
+		}).should.not.throw();
+		done();
+	});
+
 });
